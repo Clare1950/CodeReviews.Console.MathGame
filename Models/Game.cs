1c167d3e7e12c0c242ac6b6f77de54f49c367ac7
Clare Parkes <83555918+Clare1950@users.noreply.github.com>
2025-01-03 10:17:26 +0000

    Create Game.cs

---
+++ Models/Game.cs
@@ -0,0 +1,10 @@
+namespace Maths_Game.Models;
+
+internal class Game
+{
+    public string Correct { get; set; }
+    public DateTime Date { get; set; }
+    public int Num1 { get; set; }
+    public int Num2 { get; set; }
+    public string Operation { get; set; }
+};
